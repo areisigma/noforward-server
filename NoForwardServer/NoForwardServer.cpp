79d6a4eb525d55a024ecf8329130858781b3d516
areisigma <areisigma@gmail.com>
2022-10-01 07:57:16 +0200

    no pcap, only low level sockets

--- NoForwardServer/NoForwardServer.cpp
+++ NoForwardServer/NoForwardServer.cpp
@@ -4,15 +4,42 @@
 //3. Listen on choosen service for forged packet
 //4. Proxy to forward packets in OS
 
-#pragma comment(lib,  "ws2_32.lib")
+// No npcap, low level always better!
+
+#pragma region pragma comment
+
+//#pragma comment(lib,  "ws2_32.lib")
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
+
+#pragma endregion
+
+
+#pragma region include
 
 #include <iostream>
 #include <Windows.h>
-#include <winsock.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
 #include <tchar.h>
 #include <stdio.h>
+#include <string.h>
 
-#include "pcap.h"
+//#include "pcap.h"
+
+#pragma endregion
+
+
+#pragma region define
+
+#define MALLOC(x) HeapAlloc(GetProcessHeap(), 0, (x))
+#define FREE(x) HeapFree(GetProcessHeap(), 0, (x))
+
+#pragma endregion
+
+
+#pragma region proto
 
 int showServices();
 int forgePacket();
@@ -20,41 +47,120 @@ int listenService();
 int decapPacket();
 int forwardPacket();
 
-
-BOOL LoadNpcapDlls()
-{
-	TCHAR npcap_dir[512];
-	UINT len;
-	len = GetSystemDirectory(npcap_dir, 480);
-	if (!len) {
-		fprintf(stderr, "Error in GetSystemDirectory: %x", GetLastError());
-		return FALSE;
-	}
-	_tcscat_s(npcap_dir, 512, TEXT("\\Npcap"));
-	if (SetDllDirectory(npcap_dir) == 0) {
-		fprintf(stderr, "Error in SetDllDirectory: %x", GetLastError());
-		return FALSE;
-	}
-	return TRUE;
-}
+#pragma endregion
 
 
 int main()
 {
-	// Load Npcap and its functions. 
-	if (!LoadNpcapDlls())
-	{
-		fprintf(stderr, "[!] Couldn't load Npcap\n");
-		system("pause");
-		exit(1);
+	// Declare and initialize variables
+	PMIB_TCPTABLE pTcpTable;
+	DWORD dwSize = 0;
+	DWORD dwRetVal = 0;
+
+	char szLocalAddr[128];
+	char szRemoteAddr[128];
+
+	struct in_addr IpAddr;
+
+	int i;
+
+	pTcpTable = (MIB_TCPTABLE *)MALLOC(sizeof(MIB_TCPTABLE));
+	if (pTcpTable == NULL) {
+		printf("Error allocating memory\n");
+		return 1;
 	}
 
-	if (pcap_findalldevs()) {
+	dwSize = sizeof(MIB_TCPTABLE);
+	// Make an initial call to GetTcpTable to
+	// get the necessary size into the dwSize variable
+	if ((dwRetVal = GetTcpTable(pTcpTable, &dwSize, TRUE)) ==
+		ERROR_INSUFFICIENT_BUFFER) {
+		FREE(pTcpTable);
+		pTcpTable = (MIB_TCPTABLE *)MALLOC(dwSize);
+		if (pTcpTable == NULL) {
+			printf("Error allocating memory\n");
+			return 1;
+		}
+	}
+	// Make a second call to GetTcpTable to get
+	// the actual data we require
+	if ((dwRetVal = GetTcpTable(pTcpTable, &dwSize, TRUE)) == NO_ERROR) {
+		printf("\tNumber of entries: %d\n", (int)pTcpTable->dwNumEntries);
+		for (i = 0; i < (int)pTcpTable->dwNumEntries; i++) {
+			printf("\n\tTCP[%d] State: %ld - ", i,
+				pTcpTable->table[i].dwState);
+			switch (pTcpTable->table[i].dwState) {
+			case MIB_TCP_STATE_CLOSED:
+				printf("CLOSED\n");
+				break;
+			case MIB_TCP_STATE_LISTEN:
+				printf("LISTEN\n");
+				break;
+			case MIB_TCP_STATE_SYN_SENT:
+				printf("SYN-SENT\n");
+				break;
+			case MIB_TCP_STATE_SYN_RCVD:
+				printf("SYN-RECEIVED\n");
+				break;
+			case MIB_TCP_STATE_ESTAB:
+				printf("ESTABLISHED\n");
+				break;
+			case MIB_TCP_STATE_FIN_WAIT1:
+				printf("FIN-WAIT-1\n");
+				break;
+			case MIB_TCP_STATE_FIN_WAIT2:
+				printf("FIN-WAIT-2 \n");
+				break;
+			case MIB_TCP_STATE_CLOSE_WAIT:
+				printf("CLOSE-WAIT\n");
+				break;
+			case MIB_TCP_STATE_CLOSING:
+				printf("CLOSING\n");
+				break;
+			case MIB_TCP_STATE_LAST_ACK:
+				printf("LAST-ACK\n");
+				break;
+			case MIB_TCP_STATE_TIME_WAIT:
+				printf("TIME-WAIT\n");
+				break;
+			case MIB_TCP_STATE_DELETE_TCB:
+				printf("DELETE-TCB\n");
+				break;
+			default:
+				printf("UNKNOWN dwState value: %d\n", pTcpTable->table[i].dwState);
+				break;
+			}
 
+			IpAddr.S_un.S_addr = (u_long)pTcpTable->table[i].dwLocalAddr;
+			strcpy_s(szLocalAddr, sizeof(szLocalAddr), inet_ntoa(IpAddr));
+			printf("\tTCP[%d] Local Addr: %s\n", i, szLocalAddr);
+
+			printf("\tTCP[%d] Local Port: %d \n", i,
+				ntohs((u_short)pTcpTable->table[i].dwLocalPort));
+
+			IpAddr.S_un.S_addr = (u_long)pTcpTable->table[i].dwRemoteAddr;
+			strcpy_s(szRemoteAddr, sizeof(szRemoteAddr), inet_ntoa(IpAddr));
+			printf("\tTCP[%d] Remote Addr: %s\n", i, szRemoteAddr);
+
+			printf("\tTCP[%d] Remote Port: %d\n", i,
+				ntohs((u_short)pTcpTable->table[i].dwRemotePort));
+		}
+	}
+	else {
+		printf("\tGetTcpTable failed with %d\n", dwRetVal);
+		FREE(pTcpTable);
+		return 1;
 	}
 
+	if (pTcpTable != NULL) {
+		FREE(pTcpTable);
+		pTcpTable = NULL;
+	}
 
+	return 0;
 }
+
+#pragma region functions
 
 int showServices() {
 
@@ -75,3 +181,5 @@ int decapPacket() {
 int forwardPacket() {
 
 }
+
+#pragma endregion
